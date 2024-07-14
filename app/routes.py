1ad1de67021389ff2bab2c0ce9ad471d6ae29e45
gabesot69 <gabriel.sotero08@gmail.com>
2024-07-14 12:27:06 -0400

    feat: core structure, index and login templates and user table from database

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from flask import flash, redirect, render_template, url_for
+from app import app
+from app.forms import LoginForm
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {"username": "Joao"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+        {"author": {"username": "Gabriel"}, "body": "Testing!"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign in", form=form)
