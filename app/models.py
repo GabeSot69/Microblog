ec17dc9700444940a47abf125f5c82b692d685ca
gabesot69 <gabriel.sotero08@gmail.com>
2024-07-14 21:10:54 -0400

    feat: database migration

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from app import db
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
-    password_hash = so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     
     def __repr__(self):
         return f"<User {self.username}>"
